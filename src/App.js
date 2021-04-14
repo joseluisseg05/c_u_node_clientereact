e4f5058764f7538b7e55c1beecab44d6c87b4ea7
joseluisseg05 <joseluistimy@gmail.com>
2021-04-14 18:49:24 -0500

    Navegacion

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React, { Fragment } from 'react'
 /***COMPONESTES***/
 //Layout
 import Header from './components/layout/Header';
+import Navegacion from './components/layout/Navegacion';
 
 function App(){ //dentro del return es jsx por lo cual se comenta distinto
   return(
     <Fragment> {/* */}
       <Header />{/*forma de realizar el renderizado de un componente*/}
+      <div className="grid contenedor contenido-principal">
+        <Navegacion />
+        <main class="caja-contenido col-9">
+          
+        </main>
+      </div>
     </Fragment>
   )
 }
