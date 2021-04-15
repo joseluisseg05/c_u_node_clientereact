b46dd5d765bd2db859b8789b5c524f9a2c5fe984
joseluisseg05 <joseluistimy@gmail.com>
2021-04-14 21:31:25 -0500

    consumir la api

--- src/components/clients/Clientes.js
+++ src/components/clients/Clientes.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import clienteAxios from '../../config/axios';
 
 function Clientes() {
+
+  const consultarAPI = async () => {
+    const clienteConsulta = await clienteAxios.get('/clientes');
+    console.log(clienteConsulta)
+  }
+
+  useEffect( ()=> { //se carga automaticamente al cargar la pagina 
+    consultarAPI()//separara metodos para buenas practicas 
+  }, []);//para que se ejecute una vez
+
   return (
     <h2>Clientes</h2>
   )
